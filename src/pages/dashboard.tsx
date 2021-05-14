d38136cc82052b3d03da49c8fb28e275e774e68f
Diogo Sales <diogosalesdev@gmail.com>
2021-05-14 15:29:18 -0300

    Dashboard page finished

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,63 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from 'next/dynamic'
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    enabled: false
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-05-12T00:00:00',
+      '2021-05-13T00:00:00',
+      '2021-05-14T00:00:00',
+      '2021-05-15T00:00:00',
+      '2021-05-16T00:00:00',
+      '2021-05-20T00:00:00',
+      '2021-05-21T00:00:00'
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3
+    }
+  }
+}
+
+const series = [
+  { name: 'series 1', data: [31, 120, 10, 28, 51, 18, 109] }
+]
 
 export default function Dashboard() {
   return (
@@ -15,6 +72,27 @@ export default function Dashboard() {
         px='6'
       >
         <Sidebar />
+
+        <SimpleGrid flex='1' gap='4' minChildWidth='320px'align='flex-start'>
+          <Box
+            p='8'
+            bgColor='gray.800'
+            borderRadius={8}
+            pb='4'
+          >
+            <Text fontSize='lg' mb='4'>Inscritos da semana</Text>
+            <Chart options={options} series={series} type='area' height={160} />
+          </Box>
+
+          <Box
+            p='8'
+            bgColor='gray.800'
+            borderRadius={8}
+          >
+            <Text fontSize='lg' mb='4'>Taxa de abertura</Text>
+          </Box>
+
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
