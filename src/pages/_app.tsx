7e8b0426f1d68f6a6fbe7a7464adc8f52608a734
Diogo Sales <diogosalesdev@gmail.com>
2021-05-20 12:01:59 -0300

    Users list created with data fetching, mirageJs and React-Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,30 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { makeServer } from '../services/mirage'
 import { theme } from '../styles/theme'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
+
+const queryClient = new QueryClient()
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
 
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
